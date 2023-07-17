0cfb9a63484197b4fc31678efba7a11837d6f454
Cam <126632693+dreddisdead@users.noreply.github.com>
2023-07-17 11:56:12 -0400

    Create working Main and Pause Menus
    Main Menu where you can start or quit game. From game loop, pausing by pressing 'P' is now available and can access Main Menu by pressing 'M'. Returning to game from Main Menu will still have pause menu displayed until 'P' is pressed to resume game loop.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.locals import *
-from menu import CreateMenu
+from menu import MenuScreen, PauseScreen
 
 
 #set window size
@@ -24,19 +24,34 @@ pixel_small = pygame.font.SysFont('Pixeloid Sans', 20)
 pixel_large = pygame.font.SysFont('Pixeloid Sans', 24)
 
 #initialize menu
-main_menu = CreateMenu(WINDOW_WIDTH, WINDOW_HEIGHT, "Solitude", ["Play Game", "Options", "Q to Quit"])
+main_menu = MenuScreen(WINDOW_WIDTH, WINDOW_HEIGHT)
+pause_menu = PauseScreen(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+#set variables
+show_menu = True
+paused = False
+game_over = False
+fade_counter = 0
+FPS = 60
+
 
 
 def input(events):
+   global running, show_menu, paused, game_over
    for event in events:
       if event.type == pygame.QUIT:
          sys.exit(0)
       if event.type == pygame.KEYDOWN:
+         if event.key == pygame.K_SPACE:
+            show_menu = False
          if event.key == pygame.K_q:
-            sys.exit(0)
-         elif event.key == pygame.K_p:
-            #pause the game
-            pass
+            running = False
+         if not game_over:
+            if event.key == pygame.K_p:
+                  paused = not paused
+         if paused:
+            if event.key == pygame.K_m:
+               show_menu = True
          #check player choice
          elif event.key == pygame.K_w:
             pass
@@ -57,18 +72,36 @@ def draw_text(text, font, text_col, x, y):
    img = font.render(text, False, text_col)
    screen.blit(img, (x, y))
 
+#function for drawing background
+def draw_bg(bg_img):
+   screen.blit(bg_img, (0, 0))
+   
+   
 while running:
-    
+   
+   clock.tick(FPS)
    input(pygame.event.get())
             
    screen.fill((0, 0, 0))
     
-   #draw_text('Solitude', pixel_large, WHITE, WINDOW_WIDTH // 2 - 50, WINDOW_HEIGHT // 2 - 100)
-   #display menu
-   main_menu.display_menu()
-    
+   # Show menu if necessary
+   if show_menu:
+      main_menu.display_menu()
+   elif paused == True:
+      pause_menu.display_menu(screen)
+      
+   else:
+      if not paused:
+         if game_over == False:
+            #game loop here
+            draw_text('Game here', pixel_small, WHITE, 20, 20)
+      else:
+         #draw pause menu if paused
+         pause_menu.display_menu(screen)
+   
+   #update display
    pygame.display.flip()
     
-   clock.tick(60)
+   
     
 pygame.quit()
--- menu.py
+++ menu.py
@@ -4,44 +4,59 @@ from pygame.locals import *
 #initialize pygame
 pygame.init()
 
-class CreateMenu:
-    def __init__(self, width, height, title=None, options=[]):
+#create menu screen
+class MenuScreen:
+    def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.title = title
         self.screen = pygame.display.set_mode((self.width, self.height))
-        self.options = options
-        
+
     def display_menu(self):
-        #set up fonts
+        # Set up font
         title_font = pygame.font.SysFont('Pixeloid Sans', 40)
         option_font = pygame.font.SysFont('Pixeloid Sans', 20)
-        
+
         # Create title text and position on screen
-        if self.title != None:
-            title_text = title_font.render(self.title, False, (255, 255, 255))
-        else:
-            title_text = title_font.render("", False, (255, 255, 255))
+        title_text = title_font.render("Solitude", False, (255, 255, 255))
         title_text_rect = title_text.get_rect()
         title_text_rect.center = (self.width // 2, self.height // 4)
-        
+
         # Create options and position on screen
-        option1_text = option_font.render(self.options[0], False, (255, 255, 255))
+        option1_text = option_font.render("Press SPACE to Play", False, (255, 255, 255))
         option1_text_rect = option1_text.get_rect()
         option1_text_rect.center = (self.width // 2, self.height // 2)
 
-        option2_text = option_font.render(self.options[1], False, (255, 255, 255))
+        option2_text = option_font.render("Press Q to Quit", False, (255, 255, 255))
         option2_text_rect = option2_text.get_rect()
-        option2_text_rect.center = (self.width // 2, self.height // 2 + 50)
-        
-        option3_text = option_font.render(self.options[2], False, (255, 255, 255))
-        option3_text_rect = option2_text.get_rect()
-        option3_text_rect.center = (self.width // 2, self.height // 2 + 100)
-        
+        option2_text_rect.center = (self.width // 2, self.height * 3 // 4)
+
         # Draw everything on the screen
         self.screen.fill((0, 0, 0))
         self.screen.blit(title_text, title_text_rect)
         self.screen.blit(option1_text, option1_text_rect)
         self.screen.blit(option2_text, option2_text_rect)
-        self.screen.blit(option3_text, option3_text_rect)
         pygame.display.update()
+
+#create pause screen
+class PauseScreen():
+    def __init__(self, screen_width, screen_height):
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.font = pygame.font.SysFont('Pixeloid Sans', 20)
+        self.title_text = self.font.render('PAUSED', False, (255, 255, 255))
+        self.resume_text = self.font.render("Press 'P' to resume", False, (255, 255, 255))
+        self.quit_text = self.font.render("Press 'Q' to quit", False, (255, 255, 255))
+        self.mainmenu_text = self.font.render("Press 'M' to return to Main Menu", False, (255, 255, 255))
+
+    def display_menu(self, screen):
+        # Draw a transparent overlay to make the game appear paused
+        overlay = pygame.Surface((self.screen_width, self.screen_height))
+        overlay.set_alpha(128)
+        overlay.fill((0, 0, 0))
+        screen.blit(overlay, (0, 0))
+
+        # Draw the pause menu text
+        screen.blit(self.title_text, (self.screen_width//2 - self.title_text.get_width()//2, self.screen_height//3))
+        screen.blit(self.resume_text, (self.screen_width//2 - self.resume_text.get_width()//2, self.screen_height//2))
+        screen.blit(self.quit_text, (self.screen_width//2 - self.quit_text.get_width()//2, self.screen_height//2 + 50))
+        screen.blit(self.mainmenu_text, (self.screen_width//2 - self.mainmenu_text.get_width()//2, self.screen_height//2 + 100))
